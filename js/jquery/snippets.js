a9ddaae2089eeda754501e677dc25cbb15a1b7f5
Alistair Smith <aabbccsmith@users.noreply.github.com>
2017-02-27 13:26:17 +0000

    Update snippets.js

--- js/jquery/snippets.js
+++ js/jquery/snippets.js
@@ -5,3 +5,23 @@ link.addClass('current');
 setTimeout(function() {
 	link.removeClass('current');
 }, 3000);
+
+//Div full height/width of viewport
+
+// global vars
+var winWidth = $(window).width();
+var winHeight = $(window).height();
+
+// set initial div height / width
+$('div').css({
+    'width': winWidth,
+'height': winHeight,
+});
+
+// make sure div stays full width/height on resize
+$(window).resize(function(){
+    $('div').css({
+    'width': winWidth,
+    'height': winHeight,
+});
+});
